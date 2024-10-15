ecb79ec0ecff94281372014b747acdc0fc0a7124
Roman <lobanov.rd@phystech.edu>
2024-10-15 10:10:32 +0300

    Update 5-3-adc-volume (1).py

--- 5-3-adc-volume (1).py
+++ 5-3-adc-volume (1).py
@@ -1,44 +1,60 @@
 import RPi.GPIO as gpio
-import sys
-from time import sleep
+import time
+
 gpio.setmode(gpio.BCM)
-dac=[26, 19, 13, 6, 5, 11, 9, 10]
-leds=[21, 20, 16, 12, 7, 8, 25, 24]
-comp=4
-troyka=17
+ledOut= [0,0,0,0, 0,0,0,0]
+dac=[8, 11, 7, 1, 0, 5, 12, 6]
+led=[2, 3, 4, 17, 27, 22, 10, 9]
+comp = 14
+troyka = 13
+
 gpio.setup(dac, gpio.OUT)
-gpio.setup(leds, gpio.OUT)
-gpio.setup(troyka,gpio.OUT, initial=gpio.HIGH)
+gpio.setup(led, gpio.OUT)
+gpio.setup(troyka, gpio.OUT, initial=gpio.HIGH)
 gpio.setup(comp, gpio.IN)
 
-def perev(a):
-    return [int (elem) for elem in bin(a)[2:].zfill(8)]
+def dec2bin(n):
+    return[int (element) for element in bin(n)[2:].zfill(8)]
+def bin2dec(n):
+    num =0
+    s = 7
+    for i in range(8):
+        num+= n[i]*2**s
+        s-=1
+    return(num)
 
 def adc():
-    k=0
-    for i in range(7, -1, -1):
-        k+=2**i
-        gpio.output(dac, perev(k))
-        sleep(0.005)
-        if gpio.input(comp)==0:
-            k-=2**i
-    return k
-
-def volume(n):
-    n=int(n/256*10)
-    mas=[0]*8
-    for i in range(n-1):
-        mas[i]=1
-    return mas
+    DACout= [1,0,0,0, 0,0,0,0]
+    gpio.output(dac, DACout)
+    for i in range(8):
+        time.sleep(0.005)  
+        if gpio.input(comp) ==1:
+            DACout[i] = 0 
+        if i!=7:
+            DACout[i+1] = 1  
+        gpio.output(dac, DACout) 
+        time.sleep(0.005)       
+    return(DACout)  
 
 try:
     while True:
-        k=adc()
-        if k!=0:
-            gpio.output(leds, volume(k))
-            print(int(k/256*10))
+        Vn = bin2dec(adc())
+        d =(Vn+1)/256
 
-        
+        for i in range(0, int(8*(1-d))):
+            ledOut[i] = 0
+        for i in range(int(8*(1-d)), 8):
+            ledOut[i] = 1   
+
+        gpio.output(led, ledOut)
+
+        if Vn!=0:
+            print(Vn*3.6/256,"В")
+
+
+           
+
 finally:
     gpio.output(dac, 0)
+    gpio.cleanup()
     gpio.cleanup() 
