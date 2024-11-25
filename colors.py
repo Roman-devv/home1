5b4228c5a4fccbc42e5d8fe3b18c533e0f05c176
Roman <lobanov.rd@phystech.edu>
2024-11-26 02:24:39 +0300

    цвета

---
+++ colors.py
@@ -0,0 +1,7 @@
+import lightFunctions as l
+
+rgb = l.readIntensity('white.jpg', "a", "Лампа накаливания", 'Белый лист')
+rgb = l.readIntensity('red.jpg', "a", "Лампа накаливания", 'Красный лист')
+rgb = l.readIntensity('blue.jpg', "a", "Лампа накаливания", 'Синий лист')
+rgb = l.readIntensity('green.jpg', "a", "Лампа накаливания", 'Зелёный лист')
+rgb = l.readIntensity('yellow.jpg', "a", "Лампа накаливания", 'Жёлтый лист')
